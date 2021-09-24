eb61f02f68cfdbf19fb66f1d48dcc1e3f5ae9130
SerhatG35 <genc.serhat97@gmail.com>
2021-09-24 12:48:59 +0300

    Custom 404 page added

---
+++ pages/404.tsx
@@ -0,0 +1,15 @@
+import { Center } from '@chakra-ui/layout'
+
+const Custom404 = () => {
+    return (
+        <Center
+            w='100%'
+            h='100vh'
+            backgroundPosition='center'
+            backgroundRepeat='no-repeat'
+            backgroundImage='url(/404.svg)'
+        ></Center>
+    )
+}
+
+export default Custom404
